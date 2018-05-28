9b6c25b2c08ba96604e4951a5f7ad0811e938438
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-05-27 17:14:44 -0700

    build for gh-pages

--- src/components/autocompleteInput/autocompleteoption/autocompleteoption.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const AutoCompleteOption = (props) =>{
-	const remaining = props.word.slice(props.fragment.length);
-	return ( <div onClick= {()=> { props.selectCallback(props.word) }}><span>{props.fragment}</span>{remaining}</div>)
-}
-
-export default AutoCompleteOption;
